e8f69b5b6e5d019751de92b476b6c12489606430
Damian Bartosiak <bartosiak.contact@gmail.com>
2024-04-22 15:55:00 +0200

    Added links to active websites for various projects

--- components/project.tsx
+++ components/project.tsx
@@ -35,38 +35,42 @@ export default function Project({
       className="group mb-3 last:mb-0 sm:mb-8"
     >
       <section className="relative max-w-[46rem] overflow-hidden  rounded-lg border border-black/5 bg-gray-100 transition hover:bg-gray-200 dark:bg-white/10 dark:text-white dark:hover:bg-white/20 sm:h-[33rem] sm:pr-8 sm:group-even:pl-8">
-        <Link href={website} target="_blank" rel="noreferrer">
-          <div className="flex h-full flex-col px-5 pb-7 pt-4 sm:max-w-[50%] sm:pl-10 sm:pr-2 sm:pt-10 sm:group-even:ml-[18rem] ">
-            <h3 className="text-2xl font-semibold">{title}</h3>
-            <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
-              {description}
-            </p>
-            <ul className="mt-4 flex flex-wrap gap-2 sm:mt-auto">
-              {tags.map((tag, index) => (
-                <li
-                  className="rounded-full bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white dark:text-white/70"
-                  key={index}
-                >
-                  {tag}
-                </li>
-              ))}
-            </ul>
-            <div className="bottom-10 right-5 mt-5 group-even:left-5 group-even:right-[initial] md:absolute">
-              <Link
-                href={repository}
-                target="_blank"
-                className="rounded-full bg-black/[0.8] px-3 py-1 text-[0.8rem] uppercase tracking-wider text-white dark:text-white/70"
+        <div className="flex h-full flex-col px-5 pb-7 pt-4 sm:max-w-[50%] sm:pl-10 sm:pr-2 sm:pt-10 sm:group-even:ml-[18rem] ">
+          <h3 className="text-2xl font-semibold">{title}</h3>
+          <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
+            {description}
+          </p>
+          <ul className="mt-4 flex flex-wrap gap-2 sm:mt-auto">
+            {tags.map((tag, index) => (
+              <li
+                className="rounded-full bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white dark:text-white/70"
+                key={index}
               >
-                Repository
-              </Link>
-            </div>
-          </div>
+                {tag}
+              </li>
+            ))}
+          </ul>
+          {!website ? (
+            ""
+          ) : (
+            <Link href={website} target="_blank">
+              <p className="-right-5 bottom-20 mt-5 block w-48 rounded-full bg-black/[0.8] px-5 py-2 text-[1rem] uppercase tracking-wider text-white transition-transform duration-500 hover:scale-125 group-even:-left-5  group-even:right-[initial] group-even:text-right dark:text-white/70 md:absolute">
+                Live version
+              </p>
+            </Link>
+          )}
+          <Link href={repository} target="_blank">
+            <p className="-right-5 bottom-6 mt-5 block w-48 rounded-full bg-black/[0.8] px-5 py-2 text-[1rem] uppercase tracking-wider text-white transition-transform duration-500 hover:scale-125 group-even:-left-5 group-even:right-[initial] group-even:text-right dark:text-white/70 md:absolute">
+              Repository
+            </p>
+          </Link>
+        </div>
 
-          <Image
-            src={imageUrl}
-            alt="Project I worked on"
-            quality={95}
-            className="absolute -right-40 top-20 hidden w-[28.25rem] rounded-t-lg shadow-2xl transition group-even:-left-40 
+        <Image
+          src={imageUrl}
+          alt="Project I worked on"
+          quality={95}
+          className="absolute -right-40 top-16 hidden w-[28.25rem] rounded-t-lg shadow-2xl transition group-even:-left-40 
                     group-even:right-[initial]
                     group-hover:-translate-x-3
                     group-hover:translate-y-3
@@ -77,8 +81,7 @@ export default function Project({
                     group-even:group-hover:translate-y-3
 
                     group-even:group-hover:rotate-2 sm:block"
-          />
-        </Link>
+        />
       </section>
     </motion.div>
   );
